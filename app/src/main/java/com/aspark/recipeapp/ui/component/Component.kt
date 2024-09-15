4d345efb7b8950751f9d908d75a3bf2f2cfb559d
Ashutosh Gupta <68068357+Ashutoshgupta22@users.noreply.github.com>
2024-09-15 20:05:55 +0530

    remember savable to search query

--- app/src/main/java/com/aspark/recipeapp/ui/component/Component.kt
+++ app/src/main/java/com/aspark/recipeapp/ui/component/Component.kt
@@ -38,6 +38,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -197,7 +198,7 @@ fun MySearchBar(
     content: @Composable (ColumnScope.() -> Unit)
 ) {
 
-    var query by remember { mutableStateOf("") }
+    var query by rememberSaveable { mutableStateOf("") }
     val active by remember { mutableStateOf(true) }
     val focusRequester = remember { FocusRequester() }
 
